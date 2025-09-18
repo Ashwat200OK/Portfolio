871fe6ba99a526fa651f6b14f760ee0f5ef1f6d6
Ashwat200OK <ashwathaman.r6@outlook.com>
2025-09-18 23:48:15 +0530

    modified as a responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './Pages/Navbar'
+import HeroPage from './Pages/HeroPage'
 
 function App() {
 
   return (
     <>
       <Navbar/>
+      <HeroPage/>
     </>
   )
 }
