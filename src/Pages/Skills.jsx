da18a8aefa4909ffd754e56b25b5722df6a8cb5e
Ashwat200OK <ashwathaman.r6@outlook.com>
2025-09-21 13:21:22 +0530

    skill added

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -3,9 +3,17 @@ import React from 'react'
 export default function Skills() {
     return (
         <div className='h-60'>
-            <div className="overflow-hidden whitespace-nowrap w-full bg-black text-white">
-                <div className="animate-marquee inline-block">
-                    <img src="" alt="" />
+            <div className="overflow-hidden whitespace-nowrap w-full text-white">
+                <div className="animate-marquee flex flex-wrap">
+                    <img src="./front/icons8-html-5.svg" alt="html" className='skill-icons'/>
+                    <img src="./front/icons8-css-logo.svg" alt="css" className='skill-icons' />
+                    <img src="./front/icons8-javascript.svg" alt="js" className='skill-icons'/>
+                    <img src="./front/react-svgrepo-com.svg" alt="react" className='skill-icons'/>
+                    <img src="./front/redux.svg" alt="redex" className='skill-icons'/>
+                    <img src="./front/vitejs.svg" alt="vite" className='skill-icons'/>
+                    <img src="./front/tailwind-svgrepo-com.svg" alt="tailwind" className='skill-icons'/>
+                    <img src="./front/bootstrap-svgrepo-com.svg" alt="bootstrap" className='skill-icons'/>
+                    <img src="./front/material-ui-svgrepo-com.svg" alt="material-ui" className='skill-icons'/>
                 </div>
             </div>
 
